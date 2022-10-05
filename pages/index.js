d0a40a708983700b844b5d5cf04d3db4618646d1
emannet2000 <emannet2000@yahoo.com>
2022-10-05 02:25:17 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app! by Eman Manlunas" />
+        <Header title="Welcome to Doha 81 Brigade, Support Command Head Quarter" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
